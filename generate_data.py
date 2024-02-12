b815404a12b66f3b06b202370766edd374f1d4c2
刘世伟 <liushiwei@v078027.vpn.tue.nl>
2024-02-12 16:54:01 +0000

    fix llama

--- generate_data.py
+++ generate_data.py
@@ -12,10 +12,10 @@ import sys
 import os
 
 print("Loading tokenizer")
-tokenizer = AutoTokenizer.from_pretrained("baffo32/decapoda-research-llama-7B-hf")
+tokenizer = AutoTokenizer.from_pretrained("NousResearch/Llama-2-7b-hf")
 print("Tokenizer loaded!")
 print("Loading model")
-model = AutoModelForCausalLM.from_pretrained("baffo32/decapoda-research-llama-7B-hf")
+model = AutoModelForCausalLM.from_pretrained("NousResearch/Llama-2-7b-hf")
 model = model.cuda()
 print("Model loaded!")
 
